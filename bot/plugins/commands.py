127448a0ff9e091f4586bbd24d9ddb4de1b469a1
JNS <65025344+jinto4638@users.noreply.github.com>
2021-05-14 18:03:33 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -25,7 +25,7 @@ async def start(bot, update):
         
         
        
-        caption = file_caption or ' '
+        caption = file_caption if file_caption != ("" or None) else ("<code>" + file_name + "</code>")
         
         if file_type == "document":
         
