a75562786cea4ee95a2f267e79a2e900d2614996
JNS <65025344+jinto4638@users.noreply.github.com>
2021-06-13 08:10:20 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -13,7 +13,7 @@ db = Database()
 
 @Client.on_message(filters.command(["start"]) & filters.private, group=1)
 async def start(bot, update):
-        update_channel = FORCESUB_CHANNEL
+    update_channel = FORCESUB_CHANNEL
     if update_channel:
         try:
             user = await bot.get_chat_member(update_channel, update.chat.id)
